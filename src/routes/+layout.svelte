6733b5c244e16747d1d71758118d2383d3a17380
JHL-HK <valor@jianyuelab.org>
2025-10-21 13:54:05 +0900

    Feat: Update conf for dokploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import '../app.css';
-  import Header from '$components/header.svelte'
-  import Footer from '$components/footer.svelte'
+	import Header from '$components/header.svelte';
+	import Footer from '$components/footer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-  <title>JHL-HK</title>
+	<title>JHL-HK</title>
 </svelte:head>
 
-<div class="max-w-3/4 mx-auto">
-  <Header />
-  {@render children()}
-  <Footer />
+<div class="mx-auto max-w-3/4">
+	<Header />
+	{@render children()}
+	<Footer />
 </div>
